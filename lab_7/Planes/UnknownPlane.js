e619f069b3edc653f3703716d42eb9bd8191ad91
SerzhantAnton <64481403+SerzhantAnton@users.noreply.github.com>
2021-11-14 21:51:03 +0300

    Delete lab_7 directory

--- lab_7/Planes/UnknownPlane.js
+++
@@ -1,9 +0,0 @@
-const Plane = require('./Plane');
-
-class UnknownPlane extends Plane {
-
-    constructor(model, maxSpeed, maxFlightDistance, maxLoadCapacity) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-
-    }
-}
